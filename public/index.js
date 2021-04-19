e7f19a6fa5a1bbe809bba554ec3f4c75cef9f6b9
Tom Petrangelo <tdpetrangelo@gmail.com>
2021-04-18 22:41:19 -0400

    Updated express listen port

--- public/index.js
+++ public/index.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var app = express();
-var port = 3000;
+var port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
