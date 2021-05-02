9b5ac49acce93f46c2332b8af43a0cd8e94a04e9
Tom Petrangelo <tdpetrangelo@gmail.com>
2021-05-02 17:09:32 -0400

    Formatting update to index.js

--- public/index.js
+++ public/index.js
@@ -1,9 +1,9 @@
 var express = require("express");
 var app = express();
-const path = require('path');
-require('dotenv').config(({path: path.resolve(__dirname, '../.env') }));
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
 var port = process.env.PORT || 3000;
-const { Pool } = require('pg')
+const { Pool } = require("pg");
 
 const pool = new Pool({
   user: process.env.DB_USER,
@@ -11,43 +11,41 @@ const pool = new Pool({
   database: process.env.DB_DB,
   password: process.env.DB_PW,
   port: process.env.DB_PORT,
-  ssl: { rejectUnauthorized: false }
-})
+  ssl: { rejectUnauthorized: false },
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(__dirname));
 
-app.get('/premier_league_standings', (req, res) => listPremStandings(req, res));
-app.get('/f1_standings', (req, res) => listF1Standings(req, res));
-
+app.get("/premier_league_standings", (req, res) => listPremStandings(req, res));
+app.get("/f1_standings", (req, res) => listF1Standings(req, res));
 
 app.listen(port, () => {
-    console.log("Server listening on port " + port);
+  console.log("Server listening on port " + port);
 });
 
-
 async function listPremStandings(req, res) {
-    try {
-      const db = await pool.connect()
-      const result = await db.query('SELECT * FROM premier_league_standings');
-      res.send(result.rows)
-      db.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
-    }
+  try {
+    const db = await pool.connect();
+    const result = await db.query("SELECT * FROM premier_league_standings");
+    res.send(result.rows);
+    db.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
   }
+}
 
-  async function listF1Standings(req, res) {
-    try {
-      const db = await pool.connect()
-      const result = await db.query('SELECT * FROM f1_standings');
-      res.send(result.rows)
-      db.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
-    }
+async function listF1Standings(req, res) {
+  try {
+    const db = await pool.connect();
+    const result = await db.query("SELECT * FROM f1_standings");
+    res.send(result.rows);
+    db.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
   }
+}
